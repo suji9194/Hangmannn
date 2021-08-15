c141c5a7d3bc1c766b146ce21e51e52c8ea5b4a7
Tehute <82463507+Tehute@users.noreply.github.com>
2021-08-14 21:27:14 -0500

    Update hangman.js

--- js/hangman.js
+++ js/hangman.js
@@ -1,18 +1,6 @@
 var programming_languages = [
-	"python",
-	"javascript",
-	"mongodb",
-	"json",
-	"java",
-	"html",
-	"css",
-	"c",
-	"csharp",
-	"golang",
-	"kotlin",
-	"php",
-	"sql",
-	"ruby"
+	"tehut",
+
 ]
 
 let answer = '';
