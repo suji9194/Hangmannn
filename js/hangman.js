0722a7c9bcb6c7a5360a3a211234a4239a1e8e52
Tehute <82463507+Tehute@users.noreply.github.com>
2021-08-16 18:38:57 -0500

    Update hangman.js

--- js/hangman.js
+++ js/hangman.js
@@ -1,3 +1,111 @@
+<<<<<<< HEAD
+var programming_languages = [
+	"walmart",
+  "Amazon",
+  "Apple",
+  "CVS",
+  "UnitedHealth",
+  "Berkshire",
+  "McKesson",
+  "AmerisourceBergen",
+  "Alphabet",
+  "Exon",
+  "AT&T",
+  "Costoco",
+  "Cigna",
+  "Cardinal",
+  "Microsoft",
+  "Walgreen",
+  "Kroger",
+  "Home",
+  "Chase",
+  "Verizon"
+]
+
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
+
+function randomWord() {
+  answer = programming_languages[Math.floor(Math.random() * programming_languages.length)];
+}
+
+function generateButtons() {
+  let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+    `
+      <button
+        class="btn btn-lg btn-primary m-2"
+        id='` + letter + `'
+        onClick="handleGuess('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');
+
+  document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
+
+function handleGuess(chosenLetter) {
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+  if (answer.indexOf(chosenLetter) >= 0) {
+    guessedWord();
+    checkIfGameWon();
+  } else if (answer.indexOf(chosenLetter) === -1) {
+    mistakes++;
+    updateMistakes();
+    checkIfGameLost();
+    updateHangmanPicture();
+  }
+}
+
+function updateHangmanPicture() {
+  document.getElementById('hangmanPic').src = './images/' + mistakes + '.jpg';
+}
+
+function checkIfGameWon() {
+  if (wordStatus === answer) {
+    document.getElementById('keyboard').innerHTML = 'You Did It!!!';
+  }
+}
+
+function checkIfGameLost() {
+  if (mistakes === maxWrong) {
+    document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
+    document.getElementById('keyboard').innerHTML = 'Try Again!!!';
+  }
+}
+
+function guessedWord() {
+  wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+
+  document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
+function updateMistakes() {
+  document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+function reset() {
+  mistakes = 0;
+  guessed = [];
+  document.getElementById('hangmanPic').src = './images/0.jpg';
+
+  randomWord();
+  guessedWord();
+  updateMistakes();
+  generateButtons();
+}
+
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
+randomWord();
+generateButtons();
+guessedWord();
+=======
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,3 +136,4 @@
 <script src='./js/hangman.js'></script>
 </body>
 </html>
+>>>>>>> 0742be604b7a57667eca315bd1abfb3c69510829
